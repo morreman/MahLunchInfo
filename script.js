6d6fca1942ca49105d9606d35e73d7bfb424a3a4
Marten <persson_dunk@msn.com>
2016-10-27 18:03:10 +0200

    design etc

--- script.js
+++ script.js
@@ -5,110 +5,13 @@ var colors = ["#a8dba8", "#cff09e", "#79bd9a", "#3b8686"];
 var i = 0;
 
 
-function deleteUnicorn(unicorn) {
+
+function deleteRecension(unicorn) {
     return function() {
         $.ajax({
             method: "DELETE",
             url: 'http://localhost:8080' + unicorn
         });
-    }
-}
-
-function putUnicorn(unicorn) {
-    return function() {
-        var data = {}; // Datan som skickas ska vara ett Javascript-objekt
-        data.id = $('#existingUnicorn input[name=id]').val(); // Glöm inte id-numret!
-        data.name = $('#existingUnicorn input[name=name]').val();
-        data.description = $('#existingUnicorn textarea[name=description]').val();
-        data.reportedBy = $('#existingUnicorn input[name=reportedBy]').val();
-        data.spottedWhere = {};
-        data.spottedWhere.name = $('#existingUnicorn input[name="spottedWhere.name"]').val();
-        data.spottedWhere.lat = $('#existingUnicorn input[name="spottedWhere.lat"]').val();
-        data.spottedWhere.lon = $('#existingUnicorn input[name="spottedWhere.lon"]').val();
-        /*
-         * Eftersom datumväljaren inte lägger till någon tid, får vi göra det på
-         * egen hand, men bara om vi verkligen behöver det.
-         */
-        data.spottedWhen = $('#existingUnicorn input[name=spottedWhen]').val();
-        if (data.spottedWhen.length == 10) {
-            data.spottedWhen += ' 00:00:00';
-        }
-        data.image = $('#existingUnicorn input[name=image]').val();
-
-        $.ajax({ // Här görs anropet till API:t.
-                method: "PUT", // Vi använder PUT här, inte POST
-                // Här behöver vi specificera vilken enhörning som ska uppdateras
-                url: 'http://localhost:8080' + data.id,
-                data: JSON.stringify(data),
-                headers: {
-                    "Accept": "application/json"
-                }
-            })
-            .done(function(result) {
-                /*
-                 * Vi lägger till de här utskrifterna för att se vad vi skickat upp. I
-                 * nästa steg tar vi bort dem igen.
-                 */
-                console.log('Uppdaterade till följande data:');
-                console.log(JSON.stringify(data));
-            });
-    }
-}
-
-function fetchAndUpdateInfo(details) {
-    return function() {
-        $.ajax({
-                url: "http://localhost:8080",
-                headers: {
-                    "Accept": "application/json"
-                }
-            })
-            .done(function(datain) {
-                data = datain[0];
-
-                date = new Date(data['spottedWhen'].date);
-                sighting = 'Av: ' + data['reportedBy'] + ', ' +
-                    date.toLocaleDateString() +
-                    ' i ' + data['spottedWhere']['name'];
-
-                $('#unicornName').text(data['name']);
-                $('#unicornImage').html('<img src="' + data['image'] + '">');
-                $('#unicornInfo').text(data['description']);
-                $('#unicornSighting').text(sighting);
-
-                $('form').hide();
-
-                $('#existingUnicorn input[name=id]').val(data['id']);
-                $('#existingUnicorn input[name=name]').val(data['name']);
-                $('#existingUnicorn input[name=reportedBy]').val(data['reportedBy']);
-                $('#existingUnicorn input[name="spottedWhere.name"]').val(data['spottedWhere']['name']);
-                $('#existingUnicorn input[name="spottedWhere.lat"]').val(data['spottedWhere']['lat']);
-                $('#existingUnicorn input[name="spottedWhere.lon"]').val(data['spottedWhere']['lon']);
-                $('#existingUnicorn input[name=spottedWhen]').val(date.toLocaleString());
-                $('#existingUnicorn input[name=image]').val(data['image']);
-                $('#existingUnicorn [name=description]').val(data['description']);
-
-                /*
-                 * Om du har testat att klicka runt på enhörningarna i steg 11 och sedan
-                 * klickat på knapparna, ser du att varje ny inladdning av en enhörning
-                 * lägger till ett nytt anrop, så att det blir fler och fler anrop. Genom
-                 * att släppa dem efterhand uppnår vi önskat resultat. Testa gärna.
-                 */
-                $('#addUnicorn').unbind('click');
-                $('#updateUnicorn').unbind('click');
-                $('#deleteUnicorn').unbind('click');
-
-                $('#addUnicorn').click(hideFormsAndShowOne('#newUnicorn'));
-                $('#updateUnicorn').click(hideFormsAndShowOne('#existingUnicorn'));
-                $('#deleteUnicorn').click(deleteUnicorn(data['id']));
-            });
-    }
-}
-
-function hideFormsAndShowOne(formName) {
-    return function() {
-        $('form').hide();
-        $(formName).show();
     }
 }
 
@@ -121,7 +24,7 @@ $(document).ready(function() {
             }
         })
         .done(function(data) {
-            console.log(data);
+            // console.log(data);
             description = data;
         });
 
@@ -180,7 +83,7 @@ function loopit(restaurant_name, ingridients, restaurant) {
     var fat = 0;
     var counter = 0;
     var loop_counter = 0;
-    var comment;
+    var recension = {};
 
     for (var i = 0; i < ingridients.length; i++) {
         if (ingridients[i] != undefined) {
@@ -199,7 +102,6 @@ function loopit(restaurant_name, ingridients, restaurant) {
                                     loop_counter++;
 
                                     carbs += nutritions.nutrientValues.carbohydrates;
-                                    console.log(restaurant_name + " carbs just nu:" + carbs);
                                     kcal += nutritions.nutrientValues.vitaminC;
                                     magnesium += nutritions.nutrientValues.magnesium;
                                     protein += nutritions.nutrientValues.protein;
@@ -233,44 +135,69 @@ function loopit(restaurant_name, ingridients, restaurant) {
                                     if (restaurant_nutritions.f > 100) {
                                         nutri_str.push("Fett");
                                     }
-                                    console.log(nutri_str);
 
                                     if (loop_counter == j) {
+                                      var refined_nutrient_string = "";
+                                      for (var i = 0; i < nutri_str.length; i++) {
+                                          refined_nutrient_string += nutri_str[i] + " | ";
+                                      }
+
                                         if (restaurant_name == 'Restaurang Niagara') {
                                             description.forEach(function(entry) {
-                                                if (entry.name == 'Restaurang Niagara') {
-                                                    comment = entry.description;
+                                                if (entry.restaurant == 'Restaurang Niagara') {
+                                                    recension = {
+                                                        description: entry.description,
+                                                        grade: entry.grade,
+                                                        reportedBy: entry.reportedBy
+                                                    };
                                                 }
                                             });
-                                            buildDiv(restaurant_name, restaurant.Local.title, restaurant.Local.price, nutri_str, comment);
+                                            buildDiv(restaurant_name, restaurant.Local.title, restaurant.Local.price, refined_nutrient_string, recension);
                                         } else if (restaurant_name == 'Mia Maria') {
                                             description.forEach(function(entry) {
-                                                if (entry.name == 'Mia Maria') {
-                                                    comment = entry.description;
+                                                if (entry.restaurant == 'Mia Maria') {
+                                                    recension = {
+                                                        description: entry.description,
+                                                        grade: entry.grade,
+                                                        reportedBy: entry.reportedBy
+                                                    };
                                                 }
                                             });
-                                            buildDiv(restaurant_name, restaurant.Kött.title, restaurant.Kött.price, nutri_str, comment);
+                                            buildDiv(restaurant_name, restaurant.Kött.title, restaurant.Kött.price, refined_nutrient_string, recension);
                                         } else if (restaurant_name == 'Välfärden') {
                                             description.forEach(function(entry) {
-                                                if (entry.name == 'Välfärden') {
-                                                    comment = entry.description;
+                                              console.log(description);
+                                                if (entry.restaurant == 'Välfärden') {
+                                                    recension = {
+                                                        description: entry.description,
+                                                        grade: entry.grade,
+                                                        reportedBy: entry.reportedBy
+                                                    };
                                                 }
                                             });
-                                            buildDiv(restaurant_name, restaurant["1"].title, restaurant["1"].price, nutri_str, comment);
+                                            buildDiv(restaurant_name, restaurant["1"].title, restaurant["1"].price, refined_nutrient_string, recension);
                                         } else if (restaurant_name == 'Lilla Köket') {
                                             description.forEach(function(entry) {
-                                                if (entry.name == 'Lilla Köket') {
-                                                    comment = entry.description;
+                                                if (entry.restaurant == 'Lilla Köket') {
+                                                    recension = {
+                                                        description: entry.description,
+                                                        grade: entry.grade,
+                                                        reportedBy: entry.reportedBy
+                                                    };
                                                 }
                                             });
-                                            buildDiv(restaurant_name, restaurant["1"].title, restaurant["1"].price, nutri_str, comment);
+                                            buildDiv(restaurant_name, restaurant["1"].title, restaurant["1"].price, refined_nutrient_string, recension);
                                         } else if (restaurant_name == 'La Bonne Vie') {
                                             description.forEach(function(entry) {
-                                                if (entry.name == 'La Bonne Vie') {
-                                                    comment = entry.description;
+                                                if (entry.restaurant == 'La Bonne Vie') {
+                                                    recension = {
+                                                        description: entry.description,
+                                                        grade: entry.grade,
+                                                        reportedBy: entry.reportedBy
+                                                    };
                                                 }
                                             });
-                                            buildDiv(restaurant_name, restaurant["Dagens rätt"].title, restaurant["Dagens rätt"].price, nutri_str, comment);
+                                            buildDiv(restaurant_name, restaurant["Dagens rätt"].title, restaurant["Dagens rätt"].price, refined_nutrient_string, recension);
                                         }
                                     }
                                 }
@@ -293,13 +220,24 @@ function short(words) {
 }
 
 function buildDiv(restaurant, lunchtitle, price, nutrient, description) {
+    var av5 = " av 5";
     if (price == undefined || price == "") {
         price = "0";
     }
+    if (description.grade == undefined || description.grade == "" &&
+        description.reportedBy == undefined || description.reportedBy == "" &&
+        description.description == undefined || description.description == "") {
+        av5 = "";
+        description.grade = "Okänt";
+        description.reportedBy = "";
+        description.description = "";
+    }
+
     html = '<div id="' + i + '" class="row restaurant_row"><div class="col-sm-6"><h2>' + restaurant + '</h2>' +
-        '<div><p class="col-sm-8">' + lunchtitle + '</p><p class="col-sm-4">Pris: ' + price.replace(/\D/g, '') + 'kr</p>' +
-        '</div></div><div class="col-sm-3"><br><h4>Näring</h4><p>I den här rätten finns det mycket:</p><p>' + nutrient + '</p>' +
-        '</div><div class="col-sm-3"><br><h4>Betyg</h4><p>Nått diagram</p><p>' + description + '</p></div></div>';
+        '<div class= "row"><p class="col-sm-8">' + lunchtitle + '</p><p class="col-sm-4"><b>Pris: ' + price.replace(/\D/g, '') + 'kr</b></p>' +
+        '</div></div><div class="col-sm-3"><br><h4>Näring</h4><p>I den här rätten finns det mycket:</p><p><b>' + nutrient + '</b></p>' +
+        '</div><div class="col-sm-3"><br><h4>Betyg ' + description.grade + av5 + '</h4><h5>Senaste recensionen: </h5><p>' + description.description + '</p>' +
+        '<p>Postat av: ' + description.reportedBy + '</div></div>';
     var index = Math.floor(Math.random() * colors.length);
     $('#spinner').hide();
     $("#restaurant_info").append(html);
@@ -307,17 +245,14 @@ function buildDiv(restaurant, lunchtitle, price, nutrient, description) {
     i++;
 }
 
-
 $(document).ready(function() {
     $("#spinner").append("<i class='fa fa-spinner fa-spin' style='font-size:200px'></i> ");
     spanColors();
-
-
 });
+
 $(document).on('ready page:load', function() {
     spanColors();
 });
-
 
 function spanColors() {
     var span = $('.jumbotron');
